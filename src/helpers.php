a137900c3ba3b986e17c1a453a74ed5b26e8ae0f
Eduardo Guzmán Hermandez <eduardo.guzman@placetopay.com>
2020-05-03 18:10:46 -0500

    Initial commit

---
+++ src/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ShiftCS;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+function styles(Finder $finder, array $rules = []): Config
+{
+    $rules = array_merge(require __DIR__.'/laravel_rules.php', $rules);
+
+    return Config::create()
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules);
+}
