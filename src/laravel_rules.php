696ea9bd2984e8fd66766da6b2ab175d4cfdbcd3
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2020-05-30 18:08:01 -0700

    Removed phpdoc_summary

--- src/laravel_rules.php
+++ src/laravel_rules.php
@@ -90,7 +90,7 @@ return [
     'phpdoc_no_useless_inheritdoc' => true,
     'phpdoc_scalar' => true,
     'phpdoc_single_line_var_spacing' => true,
-    'phpdoc_summary' => true,
+    'phpdoc_summary' => false,
     'phpdoc_to_comment' => true,
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
